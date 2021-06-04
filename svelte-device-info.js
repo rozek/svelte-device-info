421f4399bb460fd0ecf83c4a6ddf25463c3bc114
Andreas Rozek <A.Rozek@gmx.de>
2021-06-04 15:26:47 +0200

    switches to a new API

--- svelte-device-info.js
+++ svelte-device-info.js
@@ -1,21 +1,21 @@
 "use strict";
-/**** isMobile ****/
+/**** DeviceIsMobile ****/
 // see https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
 Object.defineProperty(exports, "__esModule", { value: true });
-var isMobile = false;
+var DeviceIsMobile = false;
 (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))
-    isMobile = true; })(navigator.userAgent || navigator.vendor || window['opera']);
-/**** isPhone ****/
+    DeviceIsMobile = true; })(navigator.userAgent || navigator.vendor || window['opera']);
+/**** DeviceIsPhone ****/
 // see https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
-var isPhone = false;
-if (isMobile) {
+var DeviceIsPhone = false;
+if (DeviceIsMobile) {
     var ViewportWidth = window.innerWidth;
     var ViewportHeight = window.innerHeight;
     var smallerEdgeSize = Math.min(ViewportWidth, ViewportHeight);
     var largerEdgeSize = Math.max(ViewportWidth, ViewportHeight);
-    isPhone = (smallerEdgeSize <= 480) && (largerEdgeSize <= 896);
+    DeviceIsPhone = (smallerEdgeSize <= 480) && (largerEdgeSize <= 896);
 }
-/**** PointingAccuracy ****/
+/**** DevicePointingAccuracy ****/
 // Internet Explorer and MS/Edge are NOT supported
 var MediaMatcher = (window.matchMedia ||
     window['webkitMatchmedia'] || window['mozMatchmedia'] || window['oMatchmedia']);
@@ -27,8 +27,8 @@ var AppRunsOnLegacyTouchDevice = (!MediaQuery('(pointer:fine)') &&
     !MediaQuery('(pointer:coarse)') && !MediaQuery('-moz-touch-enabled') &&
     (('ontouchstart' in Window) || ((navigator.maxTouchPoints || 0) > 0) ||
         (/touch|android|iphone|ipod|ipad/i).test(navigator.userAgent)));
-var PointingAccuracy; // may change while running!
-function updatePointingAccuracy() {
+var DevicePointingAccuracy; // may change while running!
+function updateDevicePointingAccuracy() {
     var updatedPointingAccuracy = 'fine';
     switch (true) {
         case MediaQuery('(pointer:none)'):
@@ -40,7 +40,7 @@ function updatePointingAccuracy() {
             updatedPointingAccuracy = 'coarse';
             break;
     }
-    PointingAccuracy = updatedPointingAccuracy;
+    DevicePointingAccuracy = updatedPointingAccuracy;
     document.body.classList.remove('noPointer', 'finePointer', 'coarsePointer');
     switch (updatedPointingAccuracy) {
         case 'none':
@@ -54,7 +54,7 @@ function updatePointingAccuracy() {
             break;
     }
 }
-updatePointingAccuracy();
+updateDevicePointingAccuracy();
 var EventHandlerRegistry = [];
 /**** registerHandler ****/
 function registerHandler(Handler, onceOnly) {
@@ -84,25 +84,25 @@ function unregisterHandler(Handler) {
         unobservePointingAccuracy();
     }
 }
-/**** onDevicePointingAccuracyChanged ****/
-function onDevicePointingAccuracyChanged(Handler) {
+/**** onPointingAccuracyChanged ****/
+function onPointingAccuracyChanged(Handler) {
     registerHandler(Handler, false);
 }
-/**** onceDevicePointingAccuracyChanged ****/
-function onceDevicePointingAccuracyChanged(Handler) {
+/**** oncePointingAccuracyChanged ****/
+function oncePointingAccuracyChanged(Handler) {
     registerHandler(Handler, true);
 }
-/**** offDevicePointingAccuracyChanged ****/
-function offDevicePointingAccuracyChanged(Handler) {
+/**** offPointingAccuracyChanged ****/
+function offPointingAccuracyChanged(Handler) {
     unregisterHandler(Handler);
 }
 /**** observePointingAccuracy (e.g., on "convertibles") ****/
 var AccuracyPoller = undefined;
 function observePointingAccuracy() {
     AccuracyPoller = setInterval(function () {
-        var oldPointingAccuracy = PointingAccuracy;
-        updatePointingAccuracy();
-        if (PointingAccuracy !== oldPointingAccuracy) {
+        var oldPointingAccuracy = DevicePointingAccuracy;
+        updateDevicePointingAccuracy();
+        if (DevicePointingAccuracy !== oldPointingAccuracy) {
             invokeRegisteredHandlers();
         }
     }, 500);
@@ -117,7 +117,7 @@ function invokeRegisteredHandlers() {
     for (var i = 0, l = EventHandlerRegistry.length; i < l; i++) {
         var _a = EventHandlerRegistry[i], Handler = _a.Handler, onceOnly = _a.onceOnly;
         try {
-            Handler(PointingAccuracy);
+            Handler(DevicePointingAccuracy);
         }
         catch (Signal) {
             console.warn('PointingAccuracy observation function failed with', Signal);
@@ -164,13 +164,14 @@ if (AppRunsOnLegacyTouchDevice) {
     }
 }
 exports.default = {
-    get DeviceIsMobile() { return isMobile; },
-    get DeviceIsPhone() { return isPhone; },
-    get DeviceIsTablet() { return isMobile && !isPhone; },
-    get DeviceIsLegacyTouchDevice() { return AppRunsOnLegacyTouchDevice; },
-    get DevicePointingAccuracy() { return PointingAccuracy; },
-    onDevicePointingAccuracyChanged: onDevicePointingAccuracyChanged,
-    onceDevicePointingAccuracyChanged: onceDevicePointingAccuracyChanged,
-    offDevicePointingAccuracyChanged: offDevicePointingAccuracyChanged,
+    get isMobile() { return DeviceIsMobile; },
+    get isPhone() { return DeviceIsPhone; },
+    get isTablet() { return DeviceIsMobile && !DeviceIsPhone; },
+    get isLegacyTouchDevice() { return AppRunsOnLegacyTouchDevice; },
+    get PointingAccuracy() { return DevicePointingAccuracy; },
+    onPointingAccuracyChanged: onPointingAccuracyChanged,
+    oncePointingAccuracyChanged: oncePointingAccuracyChanged,
+    offPointingAccuracyChanged: offPointingAccuracyChanged,
     get DevicePointingAccuracyIsObserved() { return (AccuracyPoller != null); }
 };
+
