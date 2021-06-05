fd035a10fc95ea5d73bb57d85a2bae3447139ff3
Andreas Rozek <A.Rozek@gmx.de>
2021-06-05 05:21:11 +0200

    adds minification to build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './svelte-device-info.ts',
   output: {
     dir: './',
-    format: 'umd',  // builds for both Node.js and Browser
-    name:'svelte-device-info', // required for UMD modules
+    format: 'umd', // builds for both Node.js and Browser
+    name:'Device', // required for UMD modules
+    noConflict:true,
     sourcemap: true,
     exports: 'default',
   },
-  plugins: [typescript()],
+  plugins: [typescript(), terser()],
 };
