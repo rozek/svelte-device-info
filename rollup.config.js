2081c007af0570a664c4e4a7948c07a41418e52e
Andreas Rozek <A.Rozek@gmx.de>
2021-06-15 09:42:08 +0200

    change to build both ESM and UMD modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,28 @@
 // see https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/
 
+import commonjs   from '@rollup/plugin-commonjs'
+import resolve    from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './svelte-device-info.ts',
-  output: {
-    dir: './',
-    format: 'umd', // builds for both Node.js and Browser
-    name:'Device', // required for UMD modules
-    noConflict:true,
-    sourcemap: true,
-    exports: 'default',
-  },
-  plugins: [typescript(), terser()],
+  output: [
+    {
+      dir:       './',
+      format:    'umd', // builds for both Node.js and Browser
+      name:      'Device', // required for UMD modules
+      noConflict:true,
+      sourcemap: true,
+      exports:   'default',
+    },{
+      file:     './svelte-device-info.esm.js',
+      format:   'esm',
+      sourcemap:true
+    }
+  ],
+  plugins: [
+    commonjs(), resolve(), typescript(),
+    terser({ format:{ comments:false } })
+  ],
 };
