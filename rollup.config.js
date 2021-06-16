69ec846f85b8cfde63f6affd60b6642822251b82
Andreas Rozek <A.Rozek@gmx.de>
2021-06-16 17:41:46 +0200

    switch folder structure

--- rollup.config.js
+++ rollup.config.js
@@ -6,17 +6,17 @@ import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser'
 
 export default {
-  input: './svelte-device-info.ts',
+  input: './src/svelte-device-info.ts',
   output: [
     {
-      dir:       './',
+      file:     './dist/svelte-device-info.js',
       format:    'umd', // builds for both Node.js and Browser
       name:      'Device', // required for UMD modules
       noConflict:true,
       sourcemap: true,
       exports:   'default',
     },{
-      file:     './svelte-device-info.esm.js',
+      file:     './dist/svelte-device-info.esm.js',
       format:   'esm',
       sourcemap:true
     }
